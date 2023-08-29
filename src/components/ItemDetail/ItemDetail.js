afce15cd3c2cb3666e39e4971137789de3574fb2
natalirey <natalirey99@gmail.com>
2023-08-29 16:50:47 -0300

    detail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,25 +1,26 @@
-// import './ItemDetail.css'
-// import ItemCount from '../ItemCount/ItemCount'
+import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
 
-// const ItemDetail = ({id, nombre, imagen, descripcion, precio, marca, stock}) =>{
-//     return (
-//         <article className="CardItem">
-//             <header className="Header">
-//                 <h2 className="ItemHeader">{nombre}</h2>
-//             </header>
-//             <picture>
-//                 <img src={imagen} alt={nombre} className="ItemImg" />
-//             </picture>
-//             <section>
-//                 <p className="Info">Marca: {marca}</p>
-//                 <p className="Info">{descripcion}</p>
-//                 <p className="Info">U$D: {precio}</p>
-//             </section>
-//             <footer className="ItemFooter">
-//                 <ItemCount initial={1} stock={stock} onAdd= {(quantity) => console.log('cantidad agregada ')}/>
-//             </footer>
-//         </article>
-//     )
-// }
 
-// export default ItemDetail
+const ItemDetail = ({id, nombre, imagen, descripcion, precio, marca, stock}) =>{
+    return (
+        <article className="CardItem">
+            <header className="Header">
+                <h2 className="ItemHeader">{nombre}</h2>
+            </header>
+            <picture>
+                <img src={imagen} alt={nombre} className="ItemImg" />
+            </picture>
+            <section>
+                <p className="Info">Marca: {marca}</p>
+                <p className="Info">{descripcion}</p>
+                <p className="Info">U$D: {precio}</p>
+            </section>
+            <footer className="ItemFooter">
+                <ItemCount initial={1} stock={stock} onAdd= {(quantity) => console.log('cantidad agregada ')}/>
+            </footer>
+        </article>
+    )
+}
+
+export default ItemDetail
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,29 +1,29 @@
-// import './ItemDetailContainer.css'
-// import { useEffect, useState } from "react"
-// import { productById } from "../../asyncMock"
-// import ItemDetail from '../ItemDetail/ItemDetail'
-// import { useParams } from 'react-router-dom'
+import './ItemDetailContainer.css'
+import { useEffect, useState } from "react"
+import { productById } from "../../asyncMock"
+import ItemDetail from '../ItemDetail/ItemDetail'
+import { useParams } from 'react-router-dom'
 
-// const ItemDetailContainer = () => {
-//     const [product, setProduct] = useState(null)
+const ItemDetailContainer = () => {
+    const [product, setProduct] = useState(null)
 
-//     const { itemId } = useParams()
+    const { itemId } = useParams()
 
-//     useEffect(() => {
-//         productById(itemId)
-//         .then(response => {
-//             setProduct(response)
-//         })
-//         .catch(error => {
-//             console.error(error)
-//         })
-//     },[itemId])
+    useEffect(() => {
+        productById(itemId)
+        .then(response => {
+            setProduct(response)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    },[itemId])
 
-//     return(
-//         <div className="ItemDetailContainer">
-//             <ItemDetail {...product}/>
-//         </div>
-//     )
-// }
+    return(
+        <div className="ItemDetailContainer">
+            <ItemDetail {...product}/>
+        </div>
+    )
+}
 
-// export default ItemDetailContainer
+export default ItemDetailContainer
